a8d08c72294c1eac6abe3f443db56761b10583c9
batkhongor <bathongor142@gmail.com>
2020-09-17 14:24:51 +0800

    state bolno search nemew

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,31 @@
 require("@babel/polyfill");
 import Search from "./model/Search";
 
-let search = new Search("pasta");
+/**
+ * web app төлөв
+ * -хайлтын query, үр дүн
+ * -Тухай үзүүлж байгаа жор
+ * -Лайкласан жорууд
+ * -Захиалж байгаа жорын найрлаганууд
+ */
 
-search.doSearch().then((r) => console.log(r));
+const state = {};
+const controlSearch = async () => {
+  // 1. вэбээс хайлтын түлхүүр үг авна
+  const query = "pizza";
+
+  if (query) {
+    // 2. Шинээр хайлтын обектийг үүсгэж өгнө
+    state.search = new Search(query);
+
+    // 3. Хайлт хийхэд зориулж дэлгэцийг билдэнэ
+    // 4. Хайлтийг гүйцэтгэн
+    await state.search.doSearch();
+    // 5. Хайлтын үр дүнг дэлгэцэнд хэвлэн
+    console.log(state.search.result);
+  }
+};
+document.querySelector(".search").addEventListener("submit", (e) => {
+  e.preventDefault();
+  controlSearch();
+});
