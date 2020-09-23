e210b47d9355ae7e61920cc73f0b39b8e7a6974b
batkhongor <bathongor142@gmail.com>
2020-09-23 17:11:13 +0800

    search icon gargan

--- src/js/index.js
+++ src/js/index.js
@@ -1,31 +1,42 @@
 require("@babel/polyfill");
 import Search from "./model/Search";
+import { elements, renderLoader, clearLoader } from "./view/base";
+import * as searchView from "./view/searchView";
 
 /**
- * web app төлөв
- * -хайлтын query, үр дүн
- * -Тухай үзүүлж байгаа жор
- * -Лайкласан жорууд
- * -Захиалж байгаа жорын найрлаганууд
+ * Web app төлөв
+ * - Хайлтын query, үр дүн
+ * - Тухайн үзүүлж байгаа жор
+ * - Лайкласан жорууд
+ * - Захиалж байгаа жорын найрлаганууд
  */
 
 const state = {};
+
 const controlSearch = async () => {
-  // 1. вэбээс хайлтын түлхүүр үг авна
-  const query = "pizza";
+  // 1) Вэбээс хайлтын түлхүүр үгийг гаргаж авна.
+  const query = searchView.getInput();
 
   if (query) {
-    // 2. Шинээр хайлтын обектийг үүсгэж өгнө
+    // 2) Шинээр хайлтын обьектийг үүсгэж өгнө.
     state.search = new Search(query);
 
-    // 3. Хайлт хийхэд зориулж дэлгэцийг билдэнэ
-    // 4. Хайлтийг гүйцэтгэн
+    // 3) Хайлт хийхэд зориулж дэлгэцийг UI бэлтгэнэ.
+    searchView.clearSearchQuery();
+    searchView.clearSearchResult();
+    renderLoader(elements.searchResultDiv);
+
+    // 4) Хайлтыг гүйцэтгэнэ
     await state.search.doSearch();
-    // 5. Хайлтын үр дүнг дэлгэцэнд хэвлэн
-    console.log(state.search.result);
+
+    // 5) Хайлтын үр дүнг дэлгэцэнд үзүүлнэ.
+    clearLoader();
+    if (state.search.result === undefined) alert("Хайлтаар илэрцгүй...");
+    else searchView.renderRecipes(state.search.result);
   }
 };
-document.querySelector(".search").addEventListener("submit", (e) => {
+
+elements.searchForm.addEventListener("submit", (e) => {
   e.preventDefault();
   controlSearch();
 });
